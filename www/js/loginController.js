7d6aa037826c555ef068e2bcf430d56354986200
ManikandanGunasekaran <manibecse11@gmail.com>
2017-08-20 11:48:24 +0530

    update

--- www/js/loginController.js
+++ www/js/loginController.js
@@ -1,7 +1,30 @@
-app.controller('loginController', function($scope,$state) {
-  $scope.a ="page1";
-   $scope.signUp = function(){
-    alert("Button clicked");
-      $state.go("userDetails");
-    }
- })
+app.controller('loginController', function($scope, $rootScope, $state) {
+
+
+    $scope.checkLoginState = function() {
+        // FB.login(function(response) {
+        //     if (response.authResponse) {
+        //         FB.api('/me', function(response) {
+        //             console.log(response);
+        //             $rootScope.fbData = response;
+        //              $state.go("userDetails");
+        //         });
+        //     } else {
+        //         console.log('User cancelled login or did not fully authorize.');
+        //     }
+        // }, {scope: 'public_profile,email'});
+        $cordovaOauth.facebook("1330034267115606", ["email", "public_profile"], 
+        	{ redirect_uri: "http://codingscripts.com/callback" }).then(function (result) {
+alert(result.access_token);
+}, function (error) {
+alert("Error: " + error);
+});
+}
+
+    // }
+
+    $rootScope.$on('event:social-sign-in-success', function(event, userDetails){
+    	console.log(userDetails);
+    })
+
+})
